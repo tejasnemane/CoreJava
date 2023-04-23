66612e8beb7d3e7f90ebb780738e4acda0a25a1b
tejasnemane <tejasnemane03@gmail.com>
2023-04-23 14:42:21 +0530

    .

---
+++ Program2.java
@@ -0,0 +1,30 @@
+class ArrayDemo{
+
+	public static void main(String[] args){
+
+		int arr1[] = {10,20,30,40};
+
+		char arr2[] = {'A','B'};
+
+		float arr3[] = {10.5f, 21.1f};
+
+		boolean arr4[] = {true, false, true};
+
+		System.out.println(arr1[0]);
+		System.out.println(arr1[1]);
+		System.out.println(arr1[2]);
+		System.out.println(arr1[3]);
+
+		System.out.println(arr2[0]);
+		System.out.println(arr2[1]);
+
+		System.out.println(arr3[0]);
+		System.out.println(arr3[1]);
+
+		System.out.println(arr4[0]);
+		System.out.println(arr4[1]);
+		System.out.println(arr4[2]);
+
+	}
+}
+
